8a0b6d2f3e7a3beb2909e46125df2f8d73f02aba
Vitali Kergin <vkergin@gmail.com>
2025-12-06 10:17:50 +0200

    Simplify time and output to only show current time

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from datetime import datetime
 
 print("Welcome to Git Assignment")
-print(f"Current date and time: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+print(f"Current time only: {datetime.now().strftime('%H:%M:%S')}")
