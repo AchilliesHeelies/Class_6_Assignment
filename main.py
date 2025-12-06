894b6eed9d4c52c61f1ecea179fd2b8cb7db8cfc
Vitali Kergin <vkergin@gmail.com>
2025-12-06 10:15:26 +0200

    add author name to output

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from datetime import datetime
 
 print("Welcome to Git Assignment")
-print(f"Current date and time: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+print(f"Created by: Vitali Kergin")
+
