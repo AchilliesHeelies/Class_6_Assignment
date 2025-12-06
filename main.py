46b4f085e889eddc99d46d075f1c7b1e32dbd4d6
Vitali Kergin <vkergin@gmail.com>
2025-12-06 10:25:12 +0200

    Added Hello World message at the beginning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+print("Hello World")
 print("Welcome to Git Assignment")
 print(f"Created by: Vitali Kergin")
 print(f"Current time only: {datetime.now().strftime('%H:%M:%S')}")
